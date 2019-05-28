7102224baa36b12f3152d4637391ae4f2f4b8eb2
peterhyun <peterhyun1234@gmail.com>
2019-05-28 03:47:54 +0100

    magnetic sensor by wiringpi

---
+++ Magnetic/mag_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+#define MAG 4  //GPIO 23번 
+
+int main(void)
+{
+    int mag;
+
+    if(wiringPiSetup() == -1)
+        return 1;
+    pinMode(MAG, INPUT);
+    
+    while(1)
+    {
+        mag = digitalRead(MAG);
+
+        printf("mag = %d\n",mag);
+
+        delay(1000);
+
+    }
+
+    return 0;
+}
